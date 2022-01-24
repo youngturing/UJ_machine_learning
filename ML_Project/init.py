0e72c25b5c1e0d69dc36f781e8b56d442321a22c
Mikołaj Daraż <mikolajdaraz@gmail.com>
2022-01-24 23:43:19 +0100

    Added data downloading API for .csv file

---
+++ ML_Project/init.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import pandas as pd
+
+st.write("""# My first app
+""")
+
+st.write('_dupsko_')
+# First import struct module.
+# import struct
+#
+# # Return 64 means 64-bit version, return 32 means 32-bit version.
+# version = struct.calcsize("P")*8
+# print(version)
