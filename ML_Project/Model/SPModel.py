c563632e2c779c42e1a6da4f03bb09ff10f61758
youngturing <mikolajdaraz@gmail.com>
2024-01-23 10:06:11 +0100

    Updated model and app

--- ML_Project/Model/SPModel.py
+++ ML_Project/Model/SPModel.py
@@ -1,18 +1,18 @@
-import numpy as np
-import pandas as pd
-from imblearn.over_sampling import SMOTE
-from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.linear_model import LogisticRegression
-from typing import Any
-from sklearn.model_selection import train_test_split
-from sklearn import metrics
-from sklearn import preprocessing
 import pickle
+from typing import Any
+
+import pandas as pd
 import tensorflow as tf
+from imblearn.over_sampling import SMOTE
+from sklearn import metrics
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+
 
 class LoadAndTransform:
-    def load_data(path:str) -> pd.DataFrame:
+    def load_data(path: str) -> pd.DataFrame:
         """
         :param path: path to tranformed data from Stroke database
         :return: pd.Dataframe with raw data
@@ -23,46 +23,73 @@ class LoadAndTransform:
     def transform_data(raw_data: pd.DataFrame) -> Any:
         """
         :param raw_data: pd.Dataframe with raw from Stroke database
-        :return: X_train_smote: training data oversampled with SMOTE algorithm
+        :return: x_train_smote: training data oversampled with SMOTE algorithm
                  y_train_smote: training labels oversampled with SMOTE algorithm
                  y_train, y_test: test data and labels without oversampling
         """
-        data,labels = raw_data.iloc[:,:7],raw_data['stroke']
-        RANDOM_SEED = 40
-        tf.random.set_seed(RANDOM_SEED)
-        X_train, X_test, y_train, y_test = train_test_split(data, labels,
+        data, labels = raw_data.iloc[:, :10], raw_data['stroke']
+        random_seed = 40
+        tf.random.set_seed(random_seed)
+        x_train, x_test, y_train, y_test = train_test_split(data, labels,
                                                             test_size=0.3,
-                                                            random_state=RANDOM_SEED,
+                                                            random_state=random_seed,
                                                             shuffle=True,
                                                             )
         oversampled = SMOTE(random_state=0)
-        X_train_smote, y_train_smote = oversampled.fit_sample(X_train, y_train)
-        return X_train_smote, X_test, y_train_smote, y_test
+        x_train_smote, y_train_smote = oversampled.fit_sample(x_train, y_train)
+        return x_train_smote, x_test, y_train_smote, y_test
 
-class LDA_Model:
+
+class LDAModel:
     def __init__(self, x_train, x_test, y_train, y_test):
         self.x_train = x_train
         self.x_test = x_test
         self.y_train = y_train
         self.y_test = y_test
-        self.classifier_smote = LinearDiscriminantAnalysis(n_components=None, priors=None, shrinkage=None,
-                                                           solver='svd', store_covariance=False, tol=0.0001)
+        self.lda = LinearDiscriminantAnalysis(n_components=None, priors=None, shrinkage=None,
+                                              solver='svd', store_covariance=False, tol=0.0001)
+
+        self.decision_tree = DecisionTreeClassifier(ccp_alpha=0.0, class_weight=None, criterion='gini',
+                                                    max_depth=None, max_features=None, max_leaf_nodes=None,
+                                                    min_impurity_decrease=0.0, min_impurity_split=None,
+                                                    min_samples_leaf=1, min_samples_split=2,
+                                                    min_weight_fraction_leaf=0.0, presort='deprecated',
+                                                    random_state=2048, splitter='best')
+
+        self.log_reg = LogisticRegression(multi_class='ovr', max_iter=200)
+        self.models = [self.lda, self.decision_tree, self.log_reg]
+        self.names = ['stroke_model_LDA_scikit.sav', 'stroke_model_DT_scikit.sav', 'stroke_model_LogReg_scikit.sav']
 
     def train_and_save(self):
-        model = self.classifier_smote.fit(self.x_train, self.y_train)
-        filename = 'stroke_model_LDA_scikit.sav'
-        pickle.dump(model, open(filename, 'wb'))
+        for i, j in zip(self.models, self.names):
+            model = i.fit(self.x_train, self.y_train)
+            pickle.dump(model, open(j, 'wb'))
 
 
-
-if '__main__' == __name__:
+def main():
     lt = LoadAndTransform
-    raw_data = lt.load_data('../API/data/stroke_data_transformed.csv')
-    X_train, X_test, y_train, y_test = lt.transform_data(raw_data)
-    lda = LDA_Model(X_train, X_test, y_train, y_test)
+    raw_data = lt.load_data('C:/Users/miko5/Desktop/TDS/stroke_data_transformed.csv')
+    x_train, x_test, y_train, y_test = lt.transform_data(raw_data)
+    lda = LDAModel(x_train, x_test, y_train, y_test)
     lda.train_and_save()
-    loaded_model = pickle.load(open('stroke_model_LDA_scikit.sav', 'rb'))
-    y_pred = loaded_model.predict(X_test)
+    names = ['stroke_model_LDA_scikit.xlsx', 'stroke_model_DT_scikit.xlsx', 'stroke_model_LogReg_scikit.xlsx']
+    for i, j in zip(lda.names, names):
+        loaded_model = pickle.load(open(i, 'rb'))
+        y_pred = loaded_model.predict(x_test)
+        data = {'Accuracy': [metrics.accuracy_score(y_test, y_pred)],
+                'f1_score': [metrics.f1_score(y_test, y_pred)],
+                'Precision': [metrics.precision_score(y_test, y_pred)],
+                'Recall': [metrics.recall_score(y_test, y_pred)]}
+        print(data)
+        df = pd.DataFrame(data)
+        writer = pd.ExcelWriter(j, engine='openpyxl')
+        df.to_excel(writer, sheet_name='Metrics')
+        writer.save()
+        writer.close()
     print("Accuracy:", metrics.accuracy_score(y_test, y_pred))
     print("f1_score:", metrics.f1_score(y_test, y_pred))
     print("Precision:", metrics.precision_score(y_test, y_pred))
+
+
+if '__main__' == __name__:
+    main()
--- ML_Project/Web/app.py
+++ ML_Project/Web/app.py
@@ -1,15 +1,9 @@
 import numpy as np
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
-from sklearn.ensemble import RandomForestClassifier
-from matplotlib.animation import FuncAnimation
-import matplotlib.pyplot as plt
-import matplotlib.ticker as mtick
 import pickle
 import warnings
 warnings.filterwarnings('ignore')
-
 
 st.write("""
 # Stroke Prediction App
@@ -20,29 +14,36 @@ st.write(classes_df)
 #####################################################
 
 st.sidebar.header('User Input Parameters')
+gender = st.sidebar.multiselect('Gneder', options = ['Male','Female'])
 age = st.sidebar.slider('Age', 10, 99, 1)
 hypertension = st.sidebar.multiselect('Hypertension: ', options= ['Yes','No'])
 heart_disease = st.sidebar.multiselect('Heart disease: ', options= ['Yes','No'])
 ever_married = st.sidebar.multiselect('Ever married: ', options= ['Yes','No'])
 work_type = st.sidebar.multiselect('Work type: ', options= ['Private','Self-employed','Government job','Childcare','Never worked'])
+residence_type = st.sidebar.multiselect('Residence type: ', options= ['Rural','Urban'])
 avg_glucose_level = st.sidebar.slider('Average glucose level:', 30.0, 350.0, 1.0)
+bmi = st.sidebar.slider('BMI:', 15.0, 70.0, 0.5)
 smoking_status = st.sidebar.multiselect('Smoking status: ', options= ['never smoked','Unknown','formerly smoked','smokes'])
 make_predcition_button = st.sidebar.button('Load data and predict')
-data = {'age':age
-    ,'hypertension':hypertension
-    ,'heart_disease':heart_disease
-    ,'ever_married':ever_married
-    ,'work_type':work_type
-    ,'avg_glucose_level':avg_glucose_level
-    ,'smoking_status':smoking_status
+data = {
+    'gender': gender,
+    'age':age,
+    'hypertension':hypertension,
+    'heart_disease':heart_disease,
+    'ever_married':ever_married,
+    'residence_type': residence_type,
+    'work_type':work_type,
+    'avg_glucose_level':avg_glucose_level,
+    'bmi': bmi,
+    'smoking_status':smoking_status,
         }
 
 input = pd.DataFrame(data)
 
 #####################################################
 
-def get_data(age,hypertension,heart_disease,ever_married,
-             work_type,avg_glucose_level,smoking_status):
+def get_data(gender, age, hypertension, heart_disease, ever_married,
+             work_type, residence_type, avg_glucose_level, bmi, smoking_status):
     """
     :param hypertension_features: hypertension value from user input
     :param heart_disease_features: heart_disease value from user input
@@ -51,14 +52,21 @@ def get_data(age,hypertension,heart_disease,ever_married,
     :param smoking_status_features: smoking_status value from user input
     :return: dictionary with transformed data from user input
     """
+    if gender[0] == 'Male':
+        gender_features = 0
+    else:
+        gender_features = 1
+
     if hypertension[0] == 'No':
         hypertension_features = 0
     else:
         hypertension_features = 1
+
     if heart_disease[0] == 'No':
         heart_disease_features = 0
     else:
         heart_disease_features = 1
+
     if ever_married[0] == 'No':
         ever_married_features = 0
     else:
@@ -75,6 +83,11 @@ def get_data(age,hypertension,heart_disease,ever_married,
     elif work_type[0] == 'Never worked':
         work_type_features = 4
 
+    if residence_type[0] == 'Rural':
+        residence_features = 0
+    else:
+        residence_features = 1
+
     if smoking_status[0] == 'never smoked':
         smoking_status_features = 0
     elif smoking_status[0] == 'Unknown':
@@ -84,14 +97,18 @@ def get_data(age,hypertension,heart_disease,ever_married,
     elif smoking_status[0] == 'smokes':
         smoking_status_features = 3
 
-    features_for_pred = {'age': [age]
-        , 'hypertension': [hypertension_features]
-        , 'heart_disease': [heart_disease_features]
-        , 'ever_married': [ever_married_features]
-        , 'work_type': [work_type_features]
-        , 'avg_glucose_level': [avg_glucose_level]
-        , 'smoking_status': [smoking_status_features]
-                         }
+    features_for_pred = {
+        'gender': [gender_features],
+        'age': [age],
+        'hypertension': [hypertension_features],
+        'heart_disease': [heart_disease_features],
+        'ever_married': [ever_married_features],
+        'work_type': [work_type_features],
+        'residence_type': [residence_features],
+        'avg_glucose_level': [avg_glucose_level],
+        'bmi': [bmi],
+        'smoking_status': [smoking_status_features],
+    }
     return features_for_pred
 
 def show_data(data: dict, data_input: pd.DataFrame):
@@ -102,18 +119,21 @@ def show_data(data: dict, data_input: pd.DataFrame):
     """
     features = pd.DataFrame(data)
     features_for_pred_df = pd.DataFrame(features_for_pred, index=[0])
+    features_for_pred_df['gender'].astype(np.int)
     features_for_pred_df['age'].astype(np.float)
     features_for_pred_df['hypertension'].astype(np.int)
     features_for_pred_df['heart_disease'].astype(np.int)
     features_for_pred_df['ever_married'].astype(np.int)
     features_for_pred_df['work_type'].astype(np.int)
+    features_for_pred_df['residence_type'].astype(np.int)
     features_for_pred_df['avg_glucose_level'].astype(np.float)
+    features_for_pred_df['bmi'].astype(np.float)
     features_for_pred_df['smoking_status'].astype(np.int)
     st.subheader('User Input parameters:')
     st.write(data_input)
     st.subheader('Encoded User Input parameters:')
     st.write(features_for_pred_df)
-    return features_for_pred_df
+    return features
 
 def make_predcition(filename: str,features_for_pred_df: pd.DataFrame):
     """
@@ -123,21 +143,23 @@ def make_predcition(filename: str,features_for_pred_df: pd.DataFrame):
     """
     loaded_model = pickle.load(open(filename, 'rb'))
     score = loaded_model.predict(features_for_pred_df)
+    print(score)
     if score[0] == 0:
         st.subheader(f'Prediction Probability: low probality')
         st.write(score)
     else:
         st.subheader(f'Prediction Probability: plausible')
+        st.write(score)
     return score
 
 #####################################################
 
 if make_predcition_button:
     try:
-        features_for_pred = get_data(age,hypertension,heart_disease,ever_married,
-                                     work_type,avg_glucose_level,smoking_status)
+        features_for_pred = get_data(gender, age, hypertension, heart_disease, ever_married,
+                                     work_type, residence_type, avg_glucose_level, bmi, smoking_status)
         features_for_pred_df = show_data(features_for_pred,input)
-        filename = 'C:/Users/miko5/Desktop/TDS/UJ_ML/ML_Project/Model/stroke_model_LDA_scikit.sav'
+        filename = 'C:/Users/miko5/Desktop/TDS/uj-ml/ML_Project/Model/stroke_model_LogReg_scikit.sav'
         score = make_predcition(filename,features_for_pred_df)
     except ValueError as e:
         st.error(f'There is missing data in your input information. Please enter a valid input.{e}')
